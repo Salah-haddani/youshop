10c1a0030850f00052ece86fe0fe4a6f893ada40
Salah <salahabdelwahabe@gmail.com>
2024-05-02 12:54:35 +0100

    added some middlewares and productController.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import dotenv from 'dotenv';
 import express from 'express';
 dotenv.config();
-import products from './data/products.js';
+import productRoutes from './routes/productRoutes.js';
 import connectDB from './config/db.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+
 
 const port = process.env.PORT || 5000;
 connectDB(); // connect to MongoDB
@@ -12,14 +14,9 @@ app.get('/', (req, res) => {
     res.send('API is running ...');
 });
 
-app.get('/api/products', (req, res) => {
-    res.json(products);
-});
+app.use('/api/products', productRoutes);
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-});
-
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`server running on port ${port}`));
