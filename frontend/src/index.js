8aa7ceeacff97643c6196ef28427a1dcef362178
Salah <salahabdelwahabe@gmail.com>
2024-05-04 15:20:30 +0100

    added cart Slice and reducer

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,13 +6,16 @@ import {
   Route,
   RouterProvider
 } from "react-router-dom";
+import {Provider} from 'react-redux';
 //import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles/index.css';
 import './assets/styles/bootstrap.custom.css';
 import App from './App';
+import store from './store';
 import reportWebVitals from './reportWebVitals';  
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -27,7 +30,9 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
 
